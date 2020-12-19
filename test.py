13332c1eee7b1557c8fb6b5c5bbeb10ab2f3b989
hevelius <mario.perrotta@unimi.it>
2020-12-19 21:54:40 +0100

    updated

--- test.py
+++ test.py
@@ -10,9 +10,8 @@ class TestFlatten(unittest.TestCase):
         data = random_list()
         result = flatten(data)
         for i in result:
-            if isinstance(i,list):
-                self.fail("result contains sublist")
-    
+            self.assertNotIsInstance(i, list)
+        
     def test_if_result_is_list(self):
         """
         Test if result is a list
@@ -20,6 +19,15 @@ class TestFlatten(unittest.TestCase):
         data = random_list()
         result = flatten(data)
         self.assertIsInstance(result, list)
+    
+    def test_if_list_flattened(self):
+        """
+        Test if result is flattened
+        """
+        data = [[1,2,[3]],4]
+        expected = [1,2,3,4]
+        result = flatten(data)
+        self.assertListEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
